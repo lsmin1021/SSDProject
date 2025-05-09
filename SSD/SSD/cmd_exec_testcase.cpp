4d0e918798db6badfef153b818537e4b51ba7901
CHANGHEE-H <jaduwvad2@naver.com>
2025-05-09 14:27:28 +0900

    check valid lba value. [cmdExecutor Write][RED/GREAN]

--- SSD/SSD/cmd_exec_testcase.cpp
+++ SSD/SSD/cmd_exec_testcase.cpp
@@ -1,11 +1,16 @@
 #include "cmd_executor.cpp"
 #include "gmock/gmock.h"
 
-
-TEST(CommandExecutor, TC1) {
+TEST(CommandExecutor, WriteMain) {
 	CmdExecutor cmdExecutor;
 
 	cmdExecutor.write(3, "0x12341234");
 
 	EXPECT_NO_THROW(std::exception);
+}
+
+TEST(CommandExecutor, WriteOutOfLBA) {
+	CmdExecutor cmdExecutor;
+
+	EXPECT_THROW(cmdExecutor.write(101, "0x12341234"), std::exception);
 }
--- SSD/SSD/cmd_executor.cpp
+++ SSD/SSD/cmd_executor.cpp
@@ -8,11 +8,14 @@ class CmdExecutor {
 public:
 	CmdExecutor() {
 		for (int i = 0; i < 100; i++) {
-			m_ssdDevice[i] = "";
+			m_ssdDevice.push_back("");
 		}
 	}
 
 	void write(int lba, string value) {
+		if (0 > lba || 100 <= lba) {
+			throw std::exception("[WRITE ERROR] Out of lba");
+		}
 		m_ssdDevice[lba] = value;
 	}
 
