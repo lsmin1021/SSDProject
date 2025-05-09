54b52b9ed560b7b11ab0fa15f1a0ba2585261219
lsmin1021 <dltmdals826@gmail.com>
2025-05-09 16:30:55 +0900

    [refactoring] 가동성 개선을 위한 함수 위치 변경

--- SSD/SSD/command_handler.cpp
+++ SSD/SSD/command_handler.cpp
@@ -11,9 +11,8 @@ public:
 	bool isValidCommand(vector<string> cmdArr) {
 		if (isEmptyCmd(cmdArr)) return false;
 		if (isValidWriteCommand(cmdArr)) {
-			if (isValidLBA(cmdArr[1]) == false) return false;
-			if (isValidValue(cmdArr[2]) == false) return false;
-
+			if (isValidLBA(cmdArr[LBA_INDEX]) == false) return false;
+			if (isValidValue(cmdArr[VALUE_INDEX]) == false) return false;
 			return true;
 		}
 
@@ -22,33 +21,17 @@ public:
 
 	void execute(const vector<string>& cmd) {
 		CmdExecutor app;
-		if (cmd[0] == "W") {
-			app.write(std::stoi(cmd[1]), cmd[2]);
+		if (cmd[COMMAND_INDEX] == "W") {
+			app.write(std::stoi(cmd[LBA_INDEX]), cmd[VALUE_INDEX]);
 		}
 	}
 private:
-	bool isValidValue(const string& value) {
-		if (value.length() != 10) return false;
-		if (0 != value.find("0x")) return false;
-
-		for (int i = 2; i < value.length(); i++) {
-			char ch = value[i];
-			if (('0' <= ch && '9' >= ch) ||
-				('a' <= ch && 'f' >= ch) ||
-				('A' <= ch && 'F' >= ch)) {
-				continue;
-			}
-			return false;
-		}
-		return true;
+	bool isEmptyCmd(const vector<string>& cmdArr) {
+		return cmdArr.size() == 0;
 	}
 
-	bool isValidWriteCommand(const vector<string>& cmd) {
-		return cmd[0] == "W" && cmd.size() == 3;
-	}
-
-	bool isEmptyCmd(const vector<string>& cmd) {
-		return cmd.size() == 0;
+	bool isValidWriteCommand(const vector<string>& cmdArr) {
+		return cmdArr[COMMAND_INDEX] == WRITE_COMMAND && cmdArr.size() == WRITE_ARGUMENT_COUNT;
 	}
 
 	bool isValidLBA(const string& lbaString) {
@@ -67,6 +50,30 @@ private:
 		return true;
 	}
 
+	bool isValidValue(const string& valueString) {
+		if (valueString.length() != 10) return false;
+		if (0 != valueString.find("0x")) return false;
+
+		for (int i = 2; i < valueString.length(); i++) {
+			if (isValidCharcter(valueString[i])) continue;
+			return false;
+		}
+		return true;
+	}
+
+	bool isValidCharcter(char ch) {
+		return ('0' <= ch && '9' >= ch) ||
+			('a' <= ch && 'f' >= ch) ||
+			('A' <= ch && 'F' >= ch);
+	}
+
 	const int MAX_LBA = 99;
 	const int MIN_LBA = 0;
+
+	const string WRITE_COMMAND = "W";
+	const int WRITE_ARGUMENT_COUNT = 3;
+
+	const int COMMAND_INDEX = 0;
+	const int LBA_INDEX = 1;
+	const int VALUE_INDEX = 2;
 };
