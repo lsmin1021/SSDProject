a81ccdae4fa77c91297d88a495c8b500cccc3af2
lsmin1021 <dltmdals826@gmail.com>
2025-05-09 14:28:41 +0900

    [feature] write fail TC 추가

--- SSD/SSD/command_handler.cpp
+++ SSD/SSD/command_handler.cpp
@@ -10,6 +10,8 @@ public:
 	bool isValidCommand(vector<string> cmd) {
 		if (cmd[0] == "W") {
 			if (cmd.size() != 3) return false;
+			if (std::stoi(cmd[1]) >= 100) return false;
+				
 		}
 
 		return true;
--- SSD/SSD/command_handler_test_case.cpp
+++ SSD/SSD/command_handler_test_case.cpp
@@ -11,20 +11,20 @@ TEST(CommandHandler, ValidCommandCheck_Write_Success) {
 	EXPECT_EQ(expected, actual);
 }
 
-TEST(CommandHandler, ValidCommandCheck_Read_Success) {
+TEST(CommandHandler, ValidCommandCheck_Write_Fail_LessArgument) {
 	CommandHandler app;
 
-	vector<string> cmd = { "R", "0" };
+	vector<string> cmd = { "W", "0" };
 
-	bool expected = true;
+	bool expected = false;
 	bool actual = app.isValidCommand(cmd);
 	EXPECT_EQ(expected, actual);
 }
 
-TEST(CommandHandler, ValidCommandCheck_Write_Fail) {
+TEST(CommandHandler, ValidCommandCheck_Write_Fail_InvalidLBA) {
 	CommandHandler app;
 
-	vector<string> cmd = { "W", "0" };
+	vector<string> cmd = { "W", "120", "0x12345678"};
 
 	bool expected = false;
 	bool actual = app.isValidCommand(cmd);
