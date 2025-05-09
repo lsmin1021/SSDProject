350dbe363ec0e23429058489fcb9b2598d5a976a
ldy <ldy@example.com>
2025-05-09 14:33:37 +0900

    test double skeleton

--- TestShell/TestShell/ssd_interface.h
+++ TestShell/TestShell/ssd_interface.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <string>
+using std::string;
+
+class SsdInterface {
+public:
+	virtual ~SsdInterface() = default;
+	virtual void readData(string lba) = 0;
+	virtual void writeData(string lba, string data) = 0;
+};
