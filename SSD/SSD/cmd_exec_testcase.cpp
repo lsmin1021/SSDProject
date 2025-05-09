768870748f5a3fc0bd0fd484cf1650ff8178a697
CHANGHEE-H <jaduwvad2@naver.com>
2025-05-09 14:58:33 +0900

    add magic string. [cmdExecutor Read][Refactor]

--- SSD/SSD/cmd_exec_testcase.cpp
+++ SSD/SSD/cmd_exec_testcase.cpp
@@ -4,6 +4,11 @@
 class CmdExecutorFixture : public testing::Test {
 public:
 	CmdExecutor cmdExecutor;
+
+	string TEST_VALID_VALUE = "0x12341234";
+	string TEST_INVAID_VALUE_FORMET = "12341234";
+	string TEST_INVAID_VALUE_RANGE = "0x1234123er3e";
+	string TEST_INVAID_VALUE_NUMBER = "0x123.1r3e";
 };
 
 TEST_F(CmdExecutorFixture, WriteMain) {
@@ -13,19 +18,19 @@ TEST_F(CmdExecutorFixture, WriteMain) {
 }
 
 TEST_F(CmdExecutorFixture, WriteOutOfLBA) {
-	EXPECT_THROW(cmdExecutor.write(101, "0x12341234"), std::exception);
+	EXPECT_THROW(cmdExecutor.write(101, TEST_VALID_VALUE), std::exception);
 }
 
 TEST_F(CmdExecutorFixture, WriteInvalidValueFormat) {
-	EXPECT_THROW(cmdExecutor.write(3, "12341234"), std::exception);
+	EXPECT_THROW(cmdExecutor.write(3, TEST_INVAID_VALUE_FORMET), std::exception);
 }
 
 TEST_F(CmdExecutorFixture, WriteInvalidValueRange) {
-	EXPECT_THROW(cmdExecutor.write(3, "0x1234123er3e"), std::exception);
+	EXPECT_THROW(cmdExecutor.write(3, TEST_INVAID_VALUE_RANGE), std::exception);
 }
 
 TEST_F(CmdExecutorFixture, WriteInvalidValueNumber) {
-	EXPECT_THROW(cmdExecutor.write(3, "0x123.1r3e"), std::exception);
+	EXPECT_THROW(cmdExecutor.write(3, TEST_INVAID_VALUE_NUMBER), std::exception);
 }
 
 TEST_F(CmdExecutorFixture, ReadMain) {
@@ -35,3 +40,5 @@ TEST_F(CmdExecutorFixture, ReadMain) {
 
 	EXPECT_EQ("0x12341234", ret);
 }
+
+
