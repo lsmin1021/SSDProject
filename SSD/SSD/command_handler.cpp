ecea706afab01aadec60ce1ca522b410aac7d9d9
lsmin1021 <dltmdals826@gmail.com>
2025-05-09 16:02:56 +0900

    [refactoring] CommandHandler isValidCommand Refactoring을 위한 함수 추출

--- SSD/SSD/command_handler.cpp
+++ SSD/SSD/command_handler.cpp
@@ -7,28 +7,42 @@ using std::vector;
 using std::string;
 
 class CommandHandler {
+private:
+	const int MAX_LBA = 99;
+	const int MIN_LBA = 0;
 public:
-	bool isValidCommand(vector<string> cmd) {
-		if (cmd.size() == 0) return false;
-		if (cmd[0] == "W") {
-			if (cmd.size() != 3) return false;
-
-			try {
-				size_t pos = 0;
-				int a = std::stoi(cmd[1], &pos);
-				if (a > 99 || a < 0) return false;
-				if (pos != cmd[1].length())	return false;
-			}
-			catch (std::invalid_argument&) {
-				return false;
-			}
-			catch (std::out_of_range&) {
-				return false;
-			}
+	bool isValidCommand(vector<string> cmdArr) {
+		if (isEmptyCmd(cmdArr)) return false;
+		if (isValidWriteCommand(cmdArr)) {
+			if (isValidLBA(cmdArr) == false) return false;
 			return true;
 		}
 
 		return false;
+	}
+
+	bool isValidWriteCommand(vector<string>& cmd) {
+		return cmd[0] == "W" && cmd.size() == 3;
+	}
+
+	bool isEmptyCmd(vector<string>& cmd) {
+		return cmd.size() == 0;
+	}
+
+	bool isValidLBA(vector<string>& cmdArr) {
+		try {
+			size_t pos = 0;
+			int lba = std::stoi(cmdArr[1], &pos);
+			if (pos != cmdArr[1].length())	return false;
+			if (lba < MIN_LBA || lba > MAX_LBA) return false;
+		}
+		catch (std::invalid_argument&) {
+			return false;
+		}
+		catch (std::out_of_range&) {
+			return false;
+		}
+		return true;
 	}
 
 	void execute(vector<string> cmd) {
